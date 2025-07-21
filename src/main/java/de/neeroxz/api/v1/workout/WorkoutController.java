3fb36cc45e94f0c87777878405d9f86224870ab8
NeeroxZ <neeroxz@hotmail.de>
2025-07-21 12:53:08 +0200

    feat(Workout) - without content

---
+++ src/main/java/de/neeroxz/api/v1/workout/WorkoutController.java
@@ -0,0 +1,10 @@
+package de.neeroxz.api.v1.workout;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+public class WorkoutController {
+}
---
+++ src/main/java/de/neeroxz/api/v1/workout/dto/WorkoutDTO.java
@@ -0,0 +1,4 @@
+package de.neeroxz.api.v1.workout.dto;
+
+public record WorkoutDTO() {
+}
