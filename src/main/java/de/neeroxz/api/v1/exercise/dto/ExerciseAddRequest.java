b2ba83cdb4c42dc515cba8faed31e66142167a1d
NeeroxZ <neeroxz@hotmail.de>
2025-07-21 14:42:39 +0200

    dto(ExerciseAddRequest)

---
+++ src/main/java/de/neeroxz/api/v1/exercise/dto/ExerciseAddRequest.java
@@ -0,0 +1,18 @@
+package de.neeroxz.api.v1.exercise.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+public record ExerciseAddRequest(
+        @NotBlank String name,
+        @NotBlank @Size(min = 8) String description,
+        @NotNull Long categoryId,
+        @NotNull Long muscleGroupId
+) {}
