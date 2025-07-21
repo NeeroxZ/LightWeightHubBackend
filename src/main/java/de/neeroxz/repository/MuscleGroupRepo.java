a0d76276574e8b5c0cb9e8559defe13082f5cb02
NeeroxZ <neeroxz@hotmail.de>
2025-07-21 14:41:40 +0200

    repo(MuscleGroupRepo)

---
+++ src/main/java/de/neeroxz/repository/MuscleGroupRepo.java
@@ -0,0 +1,13 @@
+package de.neeroxz.repository;
+
+import de.neeroxz.domain.exercise.MuscleGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+public interface MuscleGroupRepo extends JpaRepository<MuscleGroup, Long> {}
+
