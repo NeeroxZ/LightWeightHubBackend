eb049d2662ce4a1f91584cc8642c6b4cd9314809
NeeroxZ <neeroxz@hotmail.de>
2025-07-21 12:04:31 +0200

    entity(Workout)

---
+++ src/main/java/de/neeroxz/domain/workout/Workout.java
@@ -0,0 +1,50 @@
+package de.neeroxz.domain.workout;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+import de.neeroxz.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "\"workout\"")
+public class Workout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    private String name;
+    private String description;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
