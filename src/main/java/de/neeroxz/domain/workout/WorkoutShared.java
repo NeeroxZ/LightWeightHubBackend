c4037d03394720a7d98c2d2bded5713a2c4eb891
NeeroxZ <neeroxz@hotmail.de>
2025-07-21 14:40:43 +0200

    PlaceHolder - WorkoutInvite - WorkoutService - WorkoutShared

---
+++ src/main/java/de/neeroxz/domain/workout/WorkoutShared.java
@@ -0,0 +1,10 @@
+package de.neeroxz.domain.workout;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+public class WorkoutShared {
+}
---
+++ src/main/java/de/neeroxz/service/workout/WorkoutService.java
@@ -0,0 +1,17 @@
+package de.neeroxz.service.workout;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by NeeroxZ
+ *
+ * @author : NeeroxZ
+ * @date : 21.07.2025
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class WorkoutService {
+}
