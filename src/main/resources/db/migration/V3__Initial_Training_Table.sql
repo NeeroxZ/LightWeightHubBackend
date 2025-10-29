6ce0b8425ce372dca3438c04bbb90a824b71c286
NeeroxZ <neeroxz@hotmail.de>
2025-10-29 20:38:10 +0100

    change(V3) typos

--- src/main/resources/db/migration/V3__Initial_Training_Table.sql
+++ src/main/resources/db/migration/V3__Initial_Training_Table.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS "training_session"(
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES "user"(id) on DELETE CASCADE,
-    workout_id INTEGER NOT NULL REFERENCES "Workout"(id) on DELTE CASCADE,
+    workout_id INTEGER NOT NULL REFERENCES "Workout"(id) on DELETE CASCADE,
     startet_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ended_at
+    ended_at TIMESTAMP NOT NULL
     )
 CREATE TABLE IF NOT EXISTS "training_exercise"(
 	id SERIAL PRIMARY KEY,
 	training_session_id INTEGER NOT NULL REFERENCES "training_session"(id) ON DELTE CASCADE,
-	exercise_id INTEGER NOT NULL REFERENCES "exercise"(id) on DELTE CASCADE,
+	exercise_id INTEGER NOT NULL REFERENCES "exercise"(id) on DELETE CASCADE,
 	Position INTEGER NOT NULL
 )
 CREATE TABLE IF NOT EXISTS "training_set"(
 	id SERIAL PRIMARY KEY,
-	training_exercise_id INTEGER NOT NULL REFERENCES "training_exercise"(id) ON DELTE CASCADE,
+	training_exercise_id INTEGER NOT NULL REFERENCES "training_exercise"(id) ON DELETE CASCADE,
     set_number INTEGER NOT NULL,
     reps INTEGER NOT NULL,
     weight DECIMAL(2,5) NOT NULL
