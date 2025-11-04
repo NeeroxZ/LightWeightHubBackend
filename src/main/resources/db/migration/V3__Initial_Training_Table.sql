305fdd42a6d526f2366df61382b50f27ec4450ef
NeeroxZ <neeroxz@hotmail.de>
2025-11-04 21:18:02 +0100

    FIX(V3__SQL) Typos

--- src/main/resources/db/migration/V3__Initial_Training_Table.sql
+++ src/main/resources/db/migration/V3__Initial_Training_Table.sql
@@ -1,23 +1,25 @@
-CREATE TABLE IF NOT EXISTS "training_session"(
+CREATE TABLE IF NOT EXISTS "training_session" (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES "user"(id) on DELETE CASCADE,
-    workout_id INTEGER NOT NULL REFERENCES "Workout"(id) on DELETE CASCADE,
+    workout_id INTEGER NOT NULL REFERENCES "workout"(id) on DELETE CASCADE,
     startet_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ended_at TIMESTAMP NOT NULL
-    )
-CREATE TABLE IF NOT EXISTS "training_exercise"(
+);
+
+CREATE TABLE IF NOT EXISTS "training_exercise" (
 	id SERIAL PRIMARY KEY,
-	training_session_id INTEGER NOT NULL REFERENCES "training_session"(id) ON DELTE CASCADE,
+	training_session_id INTEGER NOT NULL REFERENCES "training_session"(id) ON DELETE CASCADE,
 	exercise_id INTEGER NOT NULL REFERENCES "exercise"(id) on DELETE CASCADE,
 	Position INTEGER NOT NULL
-)
-CREATE TABLE IF NOT EXISTS "training_set"(
+);
+
+CREATE TABLE IF NOT EXISTS "training_set" (
 	id SERIAL PRIMARY KEY,
 	training_exercise_id INTEGER NOT NULL REFERENCES "training_exercise"(id) ON DELETE CASCADE,
     set_number INTEGER NOT NULL,
     reps INTEGER NOT NULL,
-    weight DECIMAL(2,5) NOT NULL
-    is_body_weight BOLEAN DEFAULT FALSE,
+    weight DECIMAL(2,5) NOT NULL,
+    is_body_weight boolean DEFAULT false,
     perfomed_at TIMESTAMP NOT NULL
 
-    )
+);
