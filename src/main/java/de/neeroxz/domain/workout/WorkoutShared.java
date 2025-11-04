44555fb3daf8253f2075077e7d4e7836a1ea19a9
NeeroxZ <neeroxz@hotmail.de>
2025-11-04 21:16:18 +0100

    feat(WorkoutShared) Add Entity

--- src/main/java/de/neeroxz/domain/workout/WorkoutShared.java
+++ src/main/java/de/neeroxz/domain/workout/WorkoutShared.java
@@ -1,10 +1,46 @@
 package de.neeroxz.domain.workout;
 
+import de.neeroxz.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
 /**
  * Created by NeeroxZ
  *
  * @author : NeeroxZ
  * @date : 21.07.2025
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "shared_workout")
 public class WorkoutShared {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "workout_id")
+    private Workout workout;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "shared_by_user_id")
+    private User sharedByUser;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "shared_at")
+    private Instant sharedAt;
 }
