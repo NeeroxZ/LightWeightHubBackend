07b2296939b4ad464b2435900782efd85d33160a
NeeroxZ <neeroxz@hotmail.de>
2025-11-04 21:16:55 +0100

    feat(Training Exercise) Add Entity

---
+++ src/main/java/de/neeroxz/domain/training/TrainingExercise.java
@@ -0,0 +1,43 @@
+package de.neeroxz.domain.training;
+
+import de.neeroxz.domain.exercise.Exercise;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "\"training_exercise\"")
+public class TrainingExercise
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long aLong;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "training_session_id", nullable = false)
+    private TrainingSession trainingSession;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+
+    @NotNull
+    @Column(name = "\"position\"", nullable = false)
+    private Integer position;
+
+}
