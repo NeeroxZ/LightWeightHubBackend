e32f5af13d7ee3e404a7055157f85f7e140062dd
NeeroxZ <neeroxz@hotmail.de>
2025-11-07 14:12:12 +0100

    feat(TrainingController) add RestController

--- src/main/java/de/neeroxz/api/v1/training/TrainingController.java
+++ src/main/java/de/neeroxz/api/v1/training/TrainingController.java
@@ -1,4 +1,27 @@
 package de.neeroxz.api.v1.training;
 
+import de.neeroxz.service.training.TrainingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/training")
+@RequiredArgsConstructor
 public class TrainingController {
+    private final TrainingService trainingService;
+
+    /*
+    Start Training
+     */
+    @GetMapping
+    public void startTraining(@PathVariable Long userId, @PathVariable Long workoutId) {
+
+    }
+
+    /*
+    Save full Training
+     */
 }
