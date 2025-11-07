17edd917ddc2cba684567a5b47eb3920b0b3c1f5
NeeroxZ <neeroxz@hotmail.de>
2025-11-07 14:16:11 +0100

    impl(WorkoutRepository) impl WorkoutRepository

--- src/main/java/de/neeroxz/repository/workout/WorkoutRepository.java
+++ src/main/java/de/neeroxz/repository/workout/WorkoutRepository.java
@@ -1,4 +1,7 @@
 package de.neeroxz.repository.workout;
 
-public interface WorkoutRepository {
+import de.neeroxz.domain.workout.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
 }
